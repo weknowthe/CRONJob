526b4291baa4616fcfcf0a609b13098524df6c38
weknowthe <vinodrathod6755@gmail.com>
2018-02-24 21:59:35 +0530

    qwe12

---
+++ restApiCron.js
@@ -0,0 +1,36 @@
+var cron = require('node-cron');
+ 
+cron.schedule('* * * * *', function(){
+  console.log('running a task every minute');
+  
+  //1.
+var http = require('http');
+ 
+var emp = [];
+ 
+//2.
+//http://rest-service.guides.spring.io/greeting
+var extServerOptions = {
+    host: 'rest-service.guides.spring.io',
+    //port: '3752',
+    path: '/greeting',
+    method: 'GET'
+};
+//3.
+function get() {
+    http.request(extServerOptions, function (res) {
+        res.setEncoding('utf8');
+        res.on('data', function (data) {
+            emp = JSON.parse(data);
+            //emp.foreach(function (e) {
+                console.log(emp.id + "\t" + emp.content );
+           // });
+        });
+ 
+    }).end();
+};
+ 
+get();
+  
+  
+});
