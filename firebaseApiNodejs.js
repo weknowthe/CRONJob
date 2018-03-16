5df26cf9e162f07ea9cbe9117f4cdff0e23c8d94
weknowthe <vinodrathod6755@gmail.com>
2018-03-16 22:59:00 +0530

    Update firebaseApiNodejs.js

--- firebaseApiNodejs.js
+++ firebaseApiNodejs.js
@@ -1,17 +1,6 @@
 var PythonShell = require('python-shell');
 
-var pyshell = new PythonShell.run('./firebaseApiCall.py');
-
-// get message back
-pyshell.on('message', function (message) {
-  // received a message sent from the Python script (a simple "print" statement) 
-  console.log("in on msg");
-  console.log(message);
-});
-
-// end the input stream and allow the process to exit 
-pyshell.end(function (err) {
-	console.log('exit');
+PythonShell.run('my_script.py', function (err) {
   if (err) throw err;
-  console.log('exit');
+  console.log('finished');
 });
