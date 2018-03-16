d50a37e95b479bc932627c2757c8f5f6e663958c
weknowthe <vinodrathod6755@gmail.com>
2018-03-16 23:01:33 +0530

    Update firebaseApiNodejs.js

--- firebaseApiNodejs.js
+++ firebaseApiNodejs.js
@@ -1,6 +1,6 @@
 var PythonShell = require('python-shell');
 
-PythonShell.run('my_script.py', function (err) {
+PythonShell.run('./firebaseApiCall.py', function (err) {
   if (err) throw err;
   console.log('finished');
 });
