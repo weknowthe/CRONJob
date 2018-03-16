fe17f39801c891237ab440c9c177a9777a4d8c22
weknowthe <vinodrathod6755@gmail.com>
2018-03-16 23:31:12 +0530

    Update firebaseApiNodejs.js

--- firebaseApiNodejs.js
+++ firebaseApiNodejs.js
@@ -8,21 +8,20 @@ var pyshell = new PythonShell.run('./firebaseApiCall.py');
 // get message back
 pyshell.on('message', function (message) {
   // received a message sent from the Python script (a simple "print" statement) 
-  console.log("on line 8");
+  //console.log("on line 8");
   console.log(message);
 });
 
 // end the input stream and allow the process to exit 
 pyshell.end(function (err) {
-	console.log('on line 14');
-  
-	console.log('exit');
+	//console.log('on line 14');  
+  console.log('end');
   if (err) throw err;
-  console.log('on line 18');
+  //console.log('on line 18');
   console.log('exit');
 });
 };
 cron.schedule('* * * * *', function(){
-  console.log('running a task every minute');
+  console.log('running a python function every minute');
   pythonCall();
 });
