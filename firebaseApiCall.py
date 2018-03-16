3a6a00798daff73bbd633ae412f947bb6adee81c
weknowthe <vinodrathod6755@gmail.com>
2018-03-16 22:41:23 +0530

    Update firebaseApiCall.py

--- firebaseApiCall.py
+++ firebaseApiCall.py
@@ -18,10 +18,8 @@ if time in trigger_hours:
         my_date  = date.today()
         folder   = calendar.day_name[my_date.weekday()][:3]
         random   = randint(1, 5)
-        textbase = "https://firebasestorage.googleapis.com/v0/b/mantrarepeater.appspot.com/o/images%2F"+folder.lower()+"%2Fmantra%2F"+str(rand
-om)+".txt?alt=media"
-        imgurl   = "https://firebasestorage.googleapis.com/v0/b/mantrarepeater.appspot.com/o/images%2F"+folder.lower()+"%2Fimg%2F"+str(random)
-+".jpg?alt=media"
+        textbase = "https://firebasestorage.googleapis.com/v0/b/mantrarepeater.appspot.com/o/images%2F"+folder.lower()+"%2Fmantra%2F"+str(random)+".txt?alt=media"
+        imgurl   = "https://firebasestorage.googleapis.com/v0/b/mantrarepeater.appspot.com/o/images%2F"+folder.lower()+"%2Fimg%2F"+str(random)+".jpg?alt=media"
         try:
                 for line in urllib2.urlopen(textbase):
                         text += line
@@ -33,14 +31,12 @@ om)+".txt?alt=media"
         except:
                 try:
                         print("trying png")
-                        imgurl  = "https://firebasestorage.googleapis.com/v0/b/mantrarepeater.appspot.com/o/images%2F"+folder.lower()+"%2Fimg%
-2F"+str(random)+".png?alt=media"
+                        imgurl  = "https://firebasestorage.googleapis.com/v0/b/mantrarepeater.appspot.com/o/images%2F"+folder.lower()+"%2Fimg%2F"+str(random)+".png?alt=media"
                         urllib2.urlopen(imgurl)
                 except:
                         try:
                                 print("trying jpeg")
-                                imgurl  = "https://firebasestorage.googleapis.com/v0/b/mantrarepeater.appspot.com/o/images%2F"+folder.lower()+
-"%2Fimg%2F"+str(random)+".jpeg?alt=media"
+                                imgurl  = "https://firebasestorage.googleapis.com/v0/b/mantrarepeater.appspot.com/o/images%2F"+folder.lower()+"%2Fimg%2F"+str(random)+".jpeg?alt=media"
                                 urllib2.urlopen(imgurl)
                         except:
                                 quit()
@@ -49,8 +45,7 @@ om)+".txt?alt=media"
                 "imageLink": imgurl
         }
         message_title = "Today's Mantra..."
-        push_service = FCMNotification(api_key="AAAAgJxP-K4:APA91bHO-ZG61ElfY5WSGtoBS8kNtNYpxfA2HIYIbWt4prC_QJJU40ouX0PypDKt_WDBUzEQGl85f5wjIC
-OBCxFYcOxwKP37J7Xoduayw2ES7jo8WSLw1V-7zcC9UXjk8R1wP10n2KES")
+        push_service = FCMNotification(api_key="AAAAgJxP-K4:APA91bHO-ZG61ElfY5WSGtoBS8kNtNYpxfA2HIYIbWt4prC_QJJU40ouX0PypDKt_WDBUzEQGl85f5wjICOBCxFYcOxwKP37J7Xoduayw2ES7jo8WSLw1V-7zcC9UXjk8R1wP10n2KES")
         topic_condition = "('imageSupport' in topics )"
         result = push_service.notify_topic_subscribers(message_body='', data_message=data_message, condition=topic_condition)
         print(result)
