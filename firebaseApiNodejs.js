3494a7b9a1a2d349415ccb6b43f7274b76eb7e2f
weknowthe <vinodrathod6755@gmail.com>
2018-03-16 22:30:19 +0530

    Add files via upload

---
+++ firebaseApiNodejs.js
@@ -0,0 +1,16 @@
+var PythonShell = require('python-shell');
+
+var pyshell = new PythonShell.run('./firebaseApiCall.py');
+
+// get message back
+pyshell.on('message', function (message) {
+  // received a message sent from the Python script (a simple "print" statement) 
+  console.log(message);
+});
+
+// end the input stream and allow the process to exit 
+pyshell.end(function (err) {
+	console.log('exit');
+  if (err) throw err;
+  console.log('exit');
+});
